fa003b7b05e72aa2a64e5fffabdb2a7f6b799ad6
Ray Wong <513172517@qq.com>
2024-03-08 14:19:43 +0800

    enhance

--- web_demo_enhanced.py
+++ web_demo_enhanced.py
@@ -1,10 +1,10 @@
-from transformers import AutoModel, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer
 import gradio as gr
 import mdtex2html
 from utils import load_model_on_gpus
 
 tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen-7B-Chat", trust_remote_code=True)
-model = AutoModel.from_pretrained("Qwen/Qwen-7B-Chat", trust_remote_code=True).cuda()
+model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen-7B-Chat", trust_remote_code=True).cuda()
 # 多显卡支持，使用下面两行代替上面一行，将num_gpus改为你实际的显卡数量
 # from utils import load_model_on_gpus
 # model = load_model_on_gpus("THUDM/chatglm2-6b", num_gpus=2)
