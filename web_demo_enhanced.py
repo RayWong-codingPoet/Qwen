a792b064fd2801bae9d9df333f13ce06ba43dd1a
Ray Wong <513172517@qq.com>
2024-03-08 15:14:44 +0800

    enhance

--- web_demo_enhanced.py
+++ web_demo_enhanced.py
@@ -1,7 +1,6 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
 import gradio as gr
 import mdtex2html
-from utils import load_model_on_gpus
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen-7B-Chat", trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen-7B-Chat", trust_remote_code=True).cuda()
@@ -55,17 +54,18 @@ def parse_text(text):
                     line = line.replace("(", "&#40;")
                     line = line.replace(")", "&#41;")
                     line = line.replace("$", "&#36;")
-                lines[i] = "<br>"+line
+                lines[i] = "<br>" + line
     text = "".join(lines)
     return text
 
 
-def predict(input, chatbot, max_length, top_p, temperature, history, past_key_values):
+def predict(input, system_prompt, chatbot, max_length, top_p, temperature, history, past_key_values):
     chatbot.append((parse_text(input), ""))
-    for response, history, past_key_values in model.chat_stream(tokenizer, input, history, past_key_values=past_key_values,
+    for response, history, past_key_values in model.chat_stream(tokenizer, input, history,
+                                                                past_key_values=past_key_values,
                                                                 return_past_key_values=True,
-                                                                max_length=max_length, top_p=top_p,
-                                                                temperature=temperature):
+                                                                max_new_tokens=max_length, top_p=top_p,
+                                                                temperature=temperature, system=system_prompt):
         chatbot[-1] = (parse_text(input), parse_text(response))
 
         yield chatbot, history, past_key_values
@@ -95,14 +95,20 @@ with gr.Blocks() as demo:
             max_length = gr.Slider(0, 32768, value=8192, step=1.0, label="Maximum length", interactive=True)
             top_p = gr.Slider(0, 1, value=0.8, step=0.01, label="Top P", interactive=True)
             temperature = gr.Slider(0, 1, value=0.95, step=0.01, label="Temperature", interactive=True)
+            system_prompt = gr.text_area(
+                label="System Prompt (Only for chat mode)",
+                height=300
+            )
 
     history = gr.State([])
     past_key_values = gr.State(None)
 
-    submitBtn.click(predict, [user_input, chatbot, max_length, top_p, temperature, history, past_key_values],
+    submitBtn.click(predict,
+                    [user_input, system_prompt, chatbot, max_length, top_p, temperature, history, past_key_values],
                     [chatbot, history, past_key_values], show_progress=True)
     submitBtn.click(reset_user_input, [], [user_input])
 
     emptyBtn.click(reset_state, outputs=[chatbot, history, past_key_values], show_progress=True)
 
-demo.queue().launch(share=False,debug=True,server_name="0.0.0.0", inbrowser=True,auth=("adminandmin", "vnljksadfu8oilrk32%@vsdaf#"))
+demo.queue().launch(share=False, debug=True, server_name="0.0.0.0", inbrowser=True,
+                    auth=("adminandmin", "vnljksadfu8oilrk32%@vsdaf#"))
